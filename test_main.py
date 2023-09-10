c7dd24d28045605a6492dad6dbaf0d59078f9fcf
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-10 17:31:14 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -8,3 +8,13 @@ from main import ExtractAndAnalyze
 
 data = ExtractAndAnalyze('https://www.kaggle.com/datasets/stefanoleone992/mutual-funds-and-etfs?select=ETF+prices.csv')
 
+# Extract the price data for the SPY ETF
+spy_prices = data['SPY']
+
+# Create a line chart
+matplotlib.pyplot.plot(spy_prices)
+matplotlib.pyplot.xlabel('Date')
+matplotlib.pyplot.ylabel('Price')
+matplotlib.pyplot.title('SPY ETF Price History')
+matplotlib.pyplot.show()
+
