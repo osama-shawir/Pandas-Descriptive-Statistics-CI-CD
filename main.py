708341730d9be71c9a863570606a2bce15a1576c
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-10 17:44:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
+
 def ExtractAndAnalyze(x):
     """This function extracts data from a csv, stores it into a dataframe, runs exploratory data analytics on it, and returns the dataframe"""
-    df = pandas.read_csv(x)
+    df = pd.read_csv(x)
     df.describe()
     return df
