0fda35019aef7e3a949ea4d3f8be7089277d1a82
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-10 21:00:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,48 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
 
-def ExtractAndAnalyze(x):
-    """This function extracts data from a csv, stores it into a dataframe, runs exploratory data analytics on it, and returns the dataframe"""
-    df = pd.read_csv(x)
+# # Pandas Descriptive Statistics Assignment
+# ## Aircraft wildlife strikes data | 1990 - 2015
+
+# In this exercise, we will extract and analyze aircraft wildlife strikes data, and we will determine the probability of each part of an aircraft getting damaged by an aircraft wildlife strike
+
+
+# Import the necessary libraries
+def main():
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import requests
+    import io
+    
+    
+    # Read our data from Google Drive
+    
+    file_id = "1TAD7Uyc9PjByt_q13uvGXGeubXnujnUi"
+    url = f"https://drive.google.com/uc?id={file_id}"
+    
+    # Download the contents of the CSV file
+    download = requests.get(url).content
+    
+    # Read the CSV file into a Pandas DataFrame
+    df = pd.read_csv(io.StringIO(download.decode("utf-8")))
+    
+    
+    # Explore the data
+    
+    df.head()
+    df.info()
     df.describe()
-    return df
+    
+    # # Now we are going to calculate the probability of each part of the flight getting damaged and plot these probabilities
+    strikes = {}
+    for c in df.columns:
+        column_name = c.split(" ")
+        # print(len(col_sep), col_sep)
+        if len(column_name) > 1 and column_name[1] == "Strike":
+            strikes[column_name[0]] = df[column_name[0] + " Damage"].sum() / df[c].sum()
+    
+    
+    # Calculate the probability of each part of the aircraft getting damaged and find the part with the highest damage probability
+    plt.bar(strikes.keys(), strikes.values())
+    plt.xticks(rotation=90)
+    plt.title("Aircraft Part Damage Probability")
+    print(max(strikes, key=strikes.get))
