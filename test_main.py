0c1da39cb159dbc4be99f761b7a3d33f0082ae0d
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-10 21:43:14 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,9 +3,20 @@
 """
 TESTS goes here
 """
-import main
+from io import StringIO
+from unittest.mock import patch
+from main import AircraftAnalytics
 
-#From main, run the analytics function
-
-def test_AircraftAnalyticcs():
-  assert(main.AircraftAnalytics == None)
+def test_AircraftAnalytics(capsys):
+    # Mock the user input for the file_id
+    with patch('builtins.input', return_value="1TAD7Uyc9PjByt_q13uvGXGeubXnujnUi"):
+        AircraftAnalytics()
+    
+    # Capture the printed output
+    captured = capsys.readouterr()
+    output = captured.out.strip()
+    
+    # Assert that some expected output is present in the printed output
+    assert "Dataframe Information" in output
+    assert "Summary Statistics" in output
+    # Add more assertions as needed
