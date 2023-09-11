f0be27c7bed3ddc03a98f73531ec0433a29d9fdc
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-10 22:30:31 -0400

    Update test_aircraft_analytics.py

--- test_aircraft_analytics.py
+++ test_aircraft_analytics.py
@@ -6,8 +6,8 @@ TESTS goes here
 from aircraft_analytics import AircraftAnalytics
 
 def test_aircraft_analytics():
+    """ This is a function to test our AircraftAnalytics Function"""
     strikes, max_damaged_part = AircraftAnalytics()
-    
     # Check if the calculated values meet expectations
     assert isinstance(strikes, dict)
     assert max_damaged_part in strikes
