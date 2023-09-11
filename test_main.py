45ae4ccb175b4ed16d8ff76261d5fdc286df9675
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-10 21:49:28 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,13 +3,12 @@
 """
 TESTS goes here
 """
-from aircraft_analytics import AircraftAnalytics  # Import your modified function
+from aircraft_analytics import AircraftAnalytics
 
 def test_aircraft_analytics():
-    # Call the function and get the results
     strikes, max_damaged_part = AircraftAnalytics()
     
-    # Check if the calculated values meet your expectations
+    # Check if the calculated values meet expectations
     assert isinstance(strikes, dict)
     assert max_damaged_part in strikes
     assert max_damaged_part == max(strikes, key=strikes.get)
