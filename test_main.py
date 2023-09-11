b8915d48bc38db648f6987cb89182ef6fdf3f862
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-10 21:02:37 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,19 +3,8 @@
 """
 TESTS goes here
 """
-import pandas as pd
-import matplotlib.pyplot as plt
-from main import ExtractAndAnalyze
+import main
 
-df = ExtractAndAnalyze('https://www.kaggle.com/datasets/stefanoleone992/mutual-funds-and-etfs?select=ETF+prices.csv')
+#From main, run the analytics function
 
-
-# Extract the price data for the SPY ETF
-spy_prices = df['SPY']
-
-# Create a line chart
-plt.plot(spy_prices)
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.title('SPY ETF Price History')
-plt.show()
+main.AircraftAnalytics()
